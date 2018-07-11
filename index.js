e7c30a254130990c22e11e1b6c5e23dde26734e3
Lucas Lago <lucasbenderlago@gmail.com>
2018-07-11 20:15:55 -0300

    Revert "Add hotfix for when execution context has no process global"
    This reverts commit 04bb6877bc133a65569e5551b40d1f92312782e4.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = new Proxy( console, {
   get: function ( target, prop ) {
-    // Hotfix for a very specific runtime where "process" is not defined
-    if(!process) return target[prop];
-    if(process.env.NODE_ENV === 'test') return () => {};
-    return target[prop];
+    return process.env.NODE_ENV === 'test' ? () => { } : target[prop];
   }
 });
