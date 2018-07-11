04bb6877bc133a65569e5551b40d1f92312782e4
Lucas Lago <lucasbenderlago@gmail.com>
2018-07-11 19:26:55 -0300

    Add hotfix for when execution context has no process global

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = new Proxy( console, {
   get: function ( target, prop ) {
-    return process.env.NODE_ENV === 'test' ? () => { } : target[prop];
+    // Hotfix for a very specific runtime where "process" is not defined
+    if(!process) return target[prop];
+    if(process.env.NODE_ENV === 'test') return () => {};
+    return target[prop];
   }
 });
